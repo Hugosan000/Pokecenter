5cdf44fcafbe9853ef7965d5332032ce3292fcf9
karin morais <andradkarin@gmail.com>
2023-05-22 08:41:17 -0300

    Creation of project

---
+++ src/test/java/poke/center/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package poke.center.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
