2bd75643e6a560ce0e7ff181f81a5bf173cc2571
karin morais <andradkarin@gmail.com>
2023-05-22 20:04:07 -0300

    Fix: fixed typo

--- src/main/resources/db/migration/V6__create_first_access_table.sql
+++ src/main/resources/db/migration/V6__create_first_access_table.sql
@@ -3,6 +3,6 @@ userId bigint NOT NULL,
 roleId bigint NOT NULL,
 firstAccess tinyint(1) NOT NULL default 0,
 PRIMARY KEY(userId, roleId),
-FOREIGN KEY (userId) REFERENCES userRole(userId)
-FOREIGN KEY (roleId) REFERENCES userRole(roleId),
+FOREIGN KEY (userId) REFERENCES userRole(userId),
+FOREIGN KEY (roleId) REFERENCES userRole(roleId)
 )
