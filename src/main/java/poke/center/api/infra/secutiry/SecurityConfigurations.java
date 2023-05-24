28473a71674d5601965f95fdd42db67bd6938f73
karin morais <andradkarin@gmail.com>
2023-05-24 19:07:20 -0300

    Feat: Added authentication manager configuration for login

--- src/main/java/poke/center/api/infra/secutiry/SecurityConfigurations.java
+++ src/main/java/poke/center/api/infra/secutiry/SecurityConfigurations.java
@@ -2,6 +2,8 @@ package poke.center.api.infra.secutiry;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,6 +19,11 @@ public class SecurityConfigurations {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
 }
