1fa29d54f9c08ecf01a1b4d9f87b6aa2a6fac0a7
karin morais <andradkarin@gmail.com>
2023-05-24 18:30:38 -0300

    feat: Created User entity

---
+++ src/main/java/poke/center/api/domain/user/User.java
@@ -0,0 +1,24 @@
+package poke.center.api.domain.user;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "user")
+@Entity(name = "User")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String login;
+    private String password
+
+}
