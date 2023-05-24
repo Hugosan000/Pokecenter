2984799d97b6cf5f54e8726dac721243d183c740
karin morais <andradkarin@gmail.com>
2023-05-24 18:46:12 -0300

    Feat: Disabled cross-site request forgery (csrf)

--- src/main/java/poke/center/api/infra/secutiry/SecurityConfigurations.java
+++ src/main/java/poke/center/api/infra/secutiry/SecurityConfigurations.java
@@ -1,12 +1,18 @@
 package poke.center.api.infra.secutiry;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
 
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .build();
+    }
 
 }
