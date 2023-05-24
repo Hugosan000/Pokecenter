824fffabacc60ba3475f3779bd0634d60f578eff
karin morais <andradkarin@gmail.com>
2023-05-24 19:32:29 -0300

    Feat: Created token service

---
+++ src/main/java/poke/center/api/infra/secutiry/TokenService.java
@@ -0,0 +1,33 @@
+package poke.center.api.infra.secutiry;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.stereotype.Service;
+import poke.center.api.domain.user.User;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    public String createToken(User user) {
+        try {
+            var algorithm = Algorithm.HMAC256("Salem");
+            return JWT.create()
+                    .withIssuer("pokecenter")
+                    .withSubject(user.getLogin())
+                    .withExpiresAt(this.expirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Error to generate token", exception);
+        }
+    }
+
+    private Instant expirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
