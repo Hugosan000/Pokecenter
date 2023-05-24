2bcd07eeb67fec9b9a10a412a2db28ebbd1219eb
karin morais <andradkarin@gmail.com>
2023-05-24 18:58:02 -0300

    Refactor: renamed login controller

--- src/main/java/poke/center/api/controller/AuthenticationController.java
+++ src/main/java/poke/center/api/controller/AuthenticationController.java
@@ -1,12 +1,14 @@
 package poke.center.api.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Login {
+@RequestMapping("/login")
+public class AuthenticationController {
 
-    @PostMapping("/login")
+    @PostMapping()
     public String login() {
         return "success";
     }
