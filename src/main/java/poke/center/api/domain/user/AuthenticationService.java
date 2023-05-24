d7b8796fc4df26bef78ace54f3dd4de7bb63869c
karin morais <andradkarin@gmail.com>
2023-05-24 18:37:44 -0300

    feat: Added validation of user for login

--- src/main/java/poke/center/api/domain/user/AuthenticationService.java
+++ src/main/java/poke/center/api/domain/user/AuthenticationService.java
@@ -1,7 +1,19 @@
 package poke.center.api.domain.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AuthenticationService {
+
+public class AuthenticationService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByLogin(username);
+    }
 }
--- src/main/java/poke/center/api/domain/user/UserRepository.java
+++ src/main/java/poke/center/api/domain/user/UserRepository.java
@@ -1,9 +1,11 @@
 package poke.center.api.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    UserDetails findByLogin(String login);
 }
