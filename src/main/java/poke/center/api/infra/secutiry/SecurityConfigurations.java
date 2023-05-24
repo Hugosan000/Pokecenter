00d986e3fd4c8bb7bd514a3073db8539cb24ada8
karin morais <andradkarin@gmail.com>
2023-05-24 18:46:35 -0300

    Feat: Session policy configured as stateless for rest api

--- src/main/java/poke/center/api/infra/secutiry/SecurityConfigurations.java
+++ src/main/java/poke/center/api/infra/secutiry/SecurityConfigurations.java
@@ -3,6 +3,7 @@ package poke.center.api.infra.secutiry;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +13,7 @@ public class SecurityConfigurations {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
 
