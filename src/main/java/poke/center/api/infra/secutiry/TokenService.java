cc24ccb4cbd1b1cc600c3cd5a8ca71406d04e4fb
karin morais <andradkarin@gmail.com>
2023-05-24 20:27:41 -0300

    Feat: created function to validate token

--- src/main/java/poke/center/api/infra/secutiry/TokenService.java
+++ src/main/java/poke/center/api/infra/secutiry/TokenService.java
@@ -3,6 +3,7 @@ package poke.center.api.infra.secutiry;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import poke.center.api.domain.user.User;
@@ -27,6 +28,20 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Error to generate token", exception);
+        }
+    }
+
+    public String getSubject(String jwtToken) {
+
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("pokecenter")
+                    .build()
+                    .verify(jwtToken)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Error to validate token", exception);
         }
     }
 
