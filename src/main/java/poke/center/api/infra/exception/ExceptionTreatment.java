3ca5f0d225a5aa8aec58af09538cba0ac2f81c53
karin morais <andradkarin@gmail.com>
2023-05-25 10:02:50 -0300

    Feat: created exception handler

---
+++ src/main/java/poke/center/api/infra/exception/ExceptionTreatment.java
@@ -0,0 +1,30 @@
+package poke.center.api.infra.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionTreatment {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity error404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(ValidationErrorData::new).toList());
+    }
+
+    private record ValidationErrorData(String field, String message) {
+        public ValidationErrorData(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
