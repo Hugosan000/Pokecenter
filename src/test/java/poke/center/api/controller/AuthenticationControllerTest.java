30a23e6078c74284c24a875a7d64ee932f9d7694
karin morais <andradkarin@gmail.com>
2023-05-25 20:03:13 -0300

    Feat: Added function to create user on test

--- src/test/java/poke/center/api/controller/AuthenticationControllerTest.java
+++ src/test/java/poke/center/api/controller/AuthenticationControllerTest.java
@@ -7,8 +7,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.junit.jupiter.api.Assertions.*;
+import poke.center.api.domain.user.UserRegisterData;
+import poke.center.api.domain.user.User;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,5 +20,19 @@ class AuthenticationControllerTest {
     @DisplayName("It should return a jwt token on successfull login")
     void loginCenario1() {
         Assertions.assertEquals(1, 1);
+    }
+
+
+    private User createUser() {
+        var user = new User(userData("test", "test", "potatopotato"));
+        return user;
+    }
+
+    private UserRegisterData userData(String name, String login, String password) {
+        return new UserRegisterData(
+                name,
+                login,
+                password
+        );
     }
 }
