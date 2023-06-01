ee1baabfb308045ee63961941b0568631e80e084
karin morais <andradkarin@gmail.com>
2023-06-01 17:25:28 -0300

    Fix: Mocked all functions login route calls to succeed on the test

--- src/test/java/poke/center/api/controller/AuthenticationControllerTest.java
+++ src/test/java/poke/center/api/controller/AuthenticationControllerTest.java
@@ -9,29 +9,31 @@ import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
-import poke.center.api.domain.role.Role;
-import poke.center.api.domain.role.RoleRepository;
 import poke.center.api.domain.user.User;
 import poke.center.api.domain.user.UserAuthenticationData;
 import poke.center.api.domain.user.UserRegisterData;
 import poke.center.api.domain.user.UserRepository;
 import poke.center.api.infra.secutiry.TokenService;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 
-@ActiveProfiles("test")
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -46,34 +48,40 @@ class AuthenticationControllerTest {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private RoleRepository roleRepository;
+    @MockBean
+    private AuthenticationManager manager;
 
-    @Autowired
-    TokenService tokenService;
+    @MockBean
+    private TokenService tokenService;
 
     @BeforeEach
     void clearDatabase(@Autowired JdbcTemplate jdbcTemplate) {
         JdbcTestUtils.deleteFromTables(jdbcTemplate, "userRole");
         JdbcTestUtils.deleteFromTables(jdbcTemplate, "user");
     }
-
     @Test
-    @DisplayName("Should")
-    void tokenScenario1() throws Exception {
+    @DisplayName("It should return a jwt token on successfull login")
+    void loginCenario1() throws Exception {
+
         var user = createUser();
-        Set<Role> userRole = new HashSet<>();
-        userRole.add(roleRepository.findByName("trainer"));
-        user.setRoles(userRole);
         userRepository.save(user);
 
-        var token = tokenService.createToken(user);
-        assertNotNull(token);
-        var response = mockMvc.perform(
-                get("/trainer/test")
-                        .header("Authorization", token)
-        ).andExpect(status().isOk());
+        Authentication auth = mock(Authentication.class);
+        auth.setAuthenticated(true);
+        when(auth.isAuthenticated()).thenReturn(true);
+        when(manager.authenticate(new UsernamePasswordAuthenticationToken(user.getLogin(), user.getPassword()))).thenReturn(auth);
+        when(auth.getPrincipal()).thenReturn(user);
+        when(tokenService.createToken((User) auth.getPrincipal())).thenReturn("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJwb2tlY2VudGVyIiwic3ViIjoibHVraW5oYSIsInJvbGVzIjpbInRyYWluZXIiXSwiZXhwIjoxNjg1NjU0MzI3fQ.aTFr0YE06EwTph6YCxzOqP3Ebc8g6FB3lC6k1LTlv5s");
 
+        var response = mockMvc.perform(
+                post("/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userAuthenticationDataJson.write(
+                                new UserAuthenticationData(user.getLogin(), user.getPassword())
+                        ).getJson())
+        ).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
     }
 
     private User createUser() {
