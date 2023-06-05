9be12a490410734c3dc5b88954a0e04e5c09cef2
karin morais <andradkarin@gmail.com>
2023-06-05 07:23:06 -0300

    Test: removed non-used dependency

--- src/test/java/poke/center/api/controller/TrainerControllerTest.java
+++ src/test/java/poke/center/api/controller/TrainerControllerTest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
