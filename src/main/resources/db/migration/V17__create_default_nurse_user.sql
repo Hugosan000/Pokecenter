4270450082bf4d3fe4a18ff539decbf5b8ffce9a
karin morais <andradkarin@gmail.com>
2023-06-05 20:40:01 -0300

    Migration: Added default nurse user

---
+++ src/main/resources/db/migration/V17__create_default_nurse_user.sql
@@ -0,0 +1,4 @@
+insert into user (name, login, password) VALUES
+("Nurse", "kingnothing", "$2a$12$NTWO1x6TZzx1tarPjvdegOZpVJVPrt2ENIY9CUpgwwsLlDnDJTvAK");
+
+insert into userRole values (LAST_INSERT_ID(), 2);
