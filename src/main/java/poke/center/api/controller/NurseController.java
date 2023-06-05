4768ce4b3e4f2d571f71ff950a7bbbb8d596f381
karin morais <andradkarin@gmail.com>
2023-06-05 17:04:54 -0300

    Feat: added begin of register for nurse

--- src/main/java/poke/center/api/controller/NurseController.java
+++ src/main/java/poke/center/api/controller/NurseController.java
@@ -1,10 +1,20 @@
 package poke.center.api.controller;
 
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import poke.center.api.domain.user.UserRegisterData;
 
 @Controller
 @RequestMapping("nurse")
 public class NurseController {
 
+
+    @PostMapping("/register")
+    public ResponseEntity nurseRegister(@RequestBody @Valid UserRegisterData user) {
+
+    }
 }
