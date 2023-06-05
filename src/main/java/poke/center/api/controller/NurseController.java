a760fde3e58b770a4e8598b495fa482e7966bb19
karin morais <andradkarin@gmail.com>
2023-06-05 07:10:36 -0300

    Feat: created nurse controller

---
+++ src/main/java/poke/center/api/controller/NurseController.java
@@ -0,0 +1,10 @@
+package poke.center.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("nurse")
+public class NurseController {
+
+}
