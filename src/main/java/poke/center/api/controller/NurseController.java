1a133f898064fa487ebcea576ab8d4ed19239cc4
karin morais <andradkarin@gmail.com>
2023-06-05 17:31:14 -0300

    Feat: Added nurse registration

--- src/main/java/poke/center/api/controller/NurseController.java
+++ src/main/java/poke/center/api/controller/NurseController.java
@@ -1,20 +1,55 @@
 package poke.center.api.controller;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import poke.center.api.domain.role.Role;
+import poke.center.api.domain.role.RoleRepository;
+import poke.center.api.domain.user.User;
 import poke.center.api.domain.user.UserRegisterData;
+import poke.center.api.domain.user.UserRepository;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Controller
 @RequestMapping("nurse")
 public class NurseController {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     @PostMapping("/register")
-    public ResponseEntity nurseRegister(@RequestBody @Valid UserRegisterData user) {
+    public ResponseEntity nurseRegister(@RequestBody @Valid UserRegisterData data) {
 
+        UserDetails user = userRepository.findByLogin(data.login());
+        if (user != null) {
+            return ResponseEntity.unprocessableEntity().body("Username not available");
+        }
+
+        Role role = roleRepository.findByName("nurse");
+        if (role == null) {
+            return ResponseEntity.badRequest().body("Invalid role");
+        }
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        var newUser = new User(data);
+        newUser.setPassword(encoder.encode(data.password()));
+
+        Set<Role> userRole = new HashSet<>();
+        userRole.add(role);
+        newUser.setRoles(userRole);
+
+        userRepository.save(newUser);
+        return ResponseEntity.noContent().build();
     }
 }
