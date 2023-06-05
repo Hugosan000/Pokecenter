e4f48c73d839722ac5bcffca3675f2ef5836162a
karin morais <andradkarin@gmail.com>
2023-06-05 07:25:38 -0300

    Test: Added test to successfully register as nurse

--- src/test/java/poke/center/api/controller/NurseControllerTest.java
+++ src/test/java/poke/center/api/controller/NurseControllerTest.java
@@ -1,7 +1,37 @@
 package poke.center.api.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import poke.center.api.domain.user.UserRegisterData;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 class NurseControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private JacksonTester<UserRegisterData> nurseRegisterDataJson;
+
+    @Test
+    @DisplayName("It should return code 204 for successfully register")
+    void nurseRegisterScenario1() throws Exception {
+
+        var response = mockMvc.perform(
+                post("/nurse/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(nurseRegisterDataJson.write(
+                                new UserRegisterData("teste", "teste", "12345678")
+                        ).getJson())
+        ).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
 
 }
