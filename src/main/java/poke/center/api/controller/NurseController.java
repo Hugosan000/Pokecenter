edac8e2827fb1a726c35a2dd5ff90e59b5b41a0a
karin morais <andradkarin@gmail.com>
2023-06-07 18:10:31 -0300

    Fix: Added rest controller configuration to nuserController show up on swagger

--- src/main/java/poke/center/api/controller/NurseController.java
+++ src/main/java/poke/center/api/controller/NurseController.java
@@ -3,10 +3,10 @@ package poke.center.api.controller;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import poke.center.api.domain.role.Role;
 import poke.center.api.domain.role.RoleRepository;
 import poke.center.api.domain.role.validation.RoleValidator;
@@ -17,7 +17,7 @@ import poke.center.api.domain.user.validation.UserValidator;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("nurse")
 public class NurseController {
 
