2bb4666745564e72b41409f7b472f99e10b97954
karin morais <andradkarin@gmail.com>
2023-06-07 19:34:55 -0300

    Feat: Added pokemon list return

--- src/main/java/poke/center/api/controller/PokemonController.java
+++ src/main/java/poke/center/api/controller/PokemonController.java
@@ -1,17 +1,24 @@
 package poke.center.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import poke.center.api.domain.pokemon.PokemonRepository;
 
 @RestController
 @RequestMapping("pokemon")
 public class PokemonController {
 
+    @Autowired
+    private PokemonRepository pokemonRepository;
 
     @GetMapping
     public ResponseEntity listPokemons() {
-        return ResponseEntity.ok().build();
+
+        var pokemons = pokemonRepository.findAll();
+
+        return ResponseEntity.ok(pokemons);
     }
 }
