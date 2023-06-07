4e69037982329da86f21e52fe64e46b5db47a66c
karin morais <andradkarin@gmail.com>
2023-06-07 19:42:39 -0300

    Fix: Fixed Pageable import

--- src/main/java/poke/center/api/controller/PokemonController.java
+++ src/main/java/poke/center/api/controller/PokemonController.java
@@ -1,11 +1,16 @@
 package poke.center.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import poke.center.api.domain.pokemon.Pokemon;
 import poke.center.api.domain.pokemon.PokemonRepository;
+
 
 @RestController
 @RequestMapping("pokemon")
@@ -15,10 +20,8 @@ public class PokemonController {
     private PokemonRepository pokemonRepository;
 
     @GetMapping
-    public ResponseEntity listPokemons() {
+    public ResponseEntity<Page<Pokemon>> listPokemons(@PageableDefault(size=150, sort = {"id"}) Pageable pagination) {
 
-        var pokemons = pokemonRepository.findAll();
-
-        return ResponseEntity.ok(pokemons);
+        return ResponseEntity.ok(pokemonRepository.findAll(pagination));
     }
 }
--- src/main/java/poke/center/api/domain/pokemon/PokemonRepository.java
+++ src/main/java/poke/center/api/domain/pokemon/PokemonRepository.java
@@ -1,11 +1,12 @@
 package poke.center.api.domain.pokemon;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
-    List<Pokemon> findAll();
+    Page<Pokemon> findAll(Pageable pagination);
 }
