4c53b53fa733f51898edc5bd2f5286c76e4d4cbc
karin morais <andradkarin@gmail.com>
2023-06-07 22:55:35 -0300

    Feat: Added JPA function to find pokemon by ID

--- src/main/java/poke/center/api/domain/pokemon/PokemonRepository.java
+++ src/main/java/poke/center/api/domain/pokemon/PokemonRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
     Page<Pokemon> findAll(Pageable pagination);
+
+    Optional<Pokemon> findById(Long id);
 }
