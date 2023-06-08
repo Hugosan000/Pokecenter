adcec109ef8dbe79d8f2418394ee597dc2b8f605
karin morais <andradkarin@gmail.com>
2023-06-07 22:55:20 -0300

    Feat: Added JPA function to find user by ID

--- src/main/java/poke/center/api/domain/user/UserRepository.java
+++ src/main/java/poke/center/api/domain/user/UserRepository.java
@@ -3,9 +3,12 @@ package poke.center.api.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     UserDetails findByLogin(String login);
+
+    Optional<User> findById(Long id);
 }
